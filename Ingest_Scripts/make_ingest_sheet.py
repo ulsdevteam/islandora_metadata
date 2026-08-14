f24180e2dfc2a0ccb5877f540fc6674bdbcf9588
Brian Gregg <bdgregg@pitt.edu>
2026-08-14 14:12:38 -0400

    Remove spurious training whitespace.

--- Ingest_Scripts/make_ingest_sheet.py
+++ Ingest_Scripts/make_ingest_sheet.py
@@ -1869,7 +1869,7 @@ def get_parent_domain(
         # Locate parent row and extract the membership column
         id_col = 'identifier' if 'identifier' in ingest_sheet.columns else 'id'
         match = ingest_sheet.loc[
-            ingest_sheet[id_col] == parent_id, 
+            ingest_sheet[id_col] == parent_id,
             'field_domain_access',
         ]
 
